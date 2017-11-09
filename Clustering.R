620d7991f304e3112ac3f7b13fe794f457e20a03
Ali <alikiyand@gmail.com>
2017-11-09 00:57:12 +0000

    adding Default dendrogram

--- Clustering.R
+++ Clustering.R
@@ -1,1 +1,17 @@
-getwd
+
+dataset <- read.csv("costpercompany.csv", header =T)
+library(cluster)
+pairs(dataset)
+plot(dataset$Fcost~ dataset$Sales, data=dataset)
+with(dataset,text(dataset$Fcost ~ dataset$Sales,labels=dataset$Company,pos=4,cex=0.6))
+#normalization
+normal = dataset[,-c(1,1)]
+means = apply(normal,2,mean)
+sds = apply(normal,2,sd)
+nor = scale(normal,center = means,scale=sds)
+#calculating Euclidean Distance
+distance = dist(nor)
+distance
+print(distance,digits = 3)
+dataset.hclust = hclust(distance)
+plot(dataset.hclust,labels = dataset$Company,main='Default from hclust')
